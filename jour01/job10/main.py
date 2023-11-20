393bdb44b704466775128db63bbfd5aad6efc8a3
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-20 11:54:58 +0100

    Modif job10

--- jour01/job10/main.py
+++ jour01/job10/main.py
@@ -1,11 +1,11 @@
 montant_initial = 11_000
-taux_rendement_annuel = 2
-gain_annuel = (montant_initial * taux_rendement_annuel / 100)
+taux_rendement_annuel = 2 / 100
+gain_annuel = (montant_initial * taux_rendement_annuel)
 print(f'Le gain annuel est de : {gain_annuel}€')
 
 montant_initial += 5_000
-taux_rendement_annuel += 2
-gain_annuel = (montant_initial * taux_rendement_annuel / 100)
+taux_rendement_annuel += (2 / 100)
+gain_annuel = (montant_initial * taux_rendement_annuel)
 print(f'Le gain annuel est de : {gain_annuel}€')
 
 # L'utilisateure retrait 10% du montant total
@@ -15,5 +15,5 @@ print(f'Le montant total est : {montant_total}€')
 montant_total -= (montant_total * 10 / 100)
 print(f'Le nouveau montant total est : {montant_total}€')
 montant_initial = montant_total
-gain_annuel -= 1
+gain_annuel -= (1 / 100)
 print(f'Le gain annuel est de : {gain_annuel}€')
