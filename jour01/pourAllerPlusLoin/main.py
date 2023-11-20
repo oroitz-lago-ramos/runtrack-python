dacf86dedc58f4521eaab4ea0bad4d204fed5d65
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-20 13:18:28 +0100

    mise a jout job10

--- jour01/pourAllerPlusLoin/main.py
+++ jour01/pourAllerPlusLoin/main.py
@@ -7,3 +7,9 @@ if count > 0:
     print(f'Il y a {count} "e" dans la chaîne de caractères')
 else:
     print("Il n'y a pas de \"e\" dans la chaîne de caractères")
+    
+# Il y a une autre methode en utilisant in
+if ("e" in chaine):
+    print('Le caractère "e" est présent')
+else :
+    print("Le caractère 'e' n'est pas présent")
