93d51ba11dd8285e78a564cce15fa209214622e3
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-20 12:32:12 +0100

    correction job10

--- jour01/job10/main.py
+++ jour01/job10/main.py
@@ -1,19 +1,21 @@
 montant_initial = 11_000
-taux_rendement_annuel = 2 / 100
-gain_annuel = (montant_initial * taux_rendement_annuel)
+taux_rendement_annuel = 2
+gain_annuel = (montant_initial * taux_rendement_annuel / 100)
+montant_total = gain_annuel + montant_initial
 print(f'Le gain annuel est de : {gain_annuel}€')
 
+# Supposons que l'utilisateur augmente le capital la même année
 montant_initial += 5_000
-taux_rendement_annuel += (2 / 100)
-gain_annuel = (montant_initial * taux_rendement_annuel)
-print(f'Le gain annuel est de : {gain_annuel}€')
+taux_rendement_annuel += 2
+gain_annuel = (montant_initial * taux_rendement_annuel / 100)
+print(f'Le nouveau gain annuel après avoir rajouté 5000€ est de : {gain_annuel}€')
 
-# L'utilisateure retrait 10% du montant total
-
+# Supposons que l'utilisateure retrait 10% du montant total après une année
 montant_total = montant_initial + gain_annuel
-print(f'Le montant total est : {montant_total}€')
+print(f'Le montant total après une année est : {montant_total}€')
 montant_total -= (montant_total * 10 / 100)
-print(f'Le nouveau montant total est : {montant_total}€')
+print(f'Le nouveau montant total après retrait est : {montant_total}€')
 montant_initial = montant_total
-gain_annuel -= (1 / 100)
-print(f'Le gain annuel est de : {gain_annuel}€')
+taux_rendement_annuel -= 1
+gain_annuel = (montant_initial * taux_rendement_annuel / 100)
+print(f'Le gain annuel est donc de : {gain_annuel}€')
