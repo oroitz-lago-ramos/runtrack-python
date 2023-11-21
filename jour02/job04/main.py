4aa9abd56d044942ae576c2cf006e3ab5cb0f7e3
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-21 10:55:05 +0100

    securité +

--- jour02/job04/main.py
+++ jour02/job04/main.py
@@ -1,9 +1,10 @@
 choix_table = int(input("Entrez un entier supérieur à zero (N): "))
-
-for i in range(1, choix_table + 1):
-    print(f"Table de multiplication de {i} :")
-    for j in range(1, 11):
-        resultat = i * j
-        print(f"{i} x {j} = {resultat}")
-    print("\n")
-    
+if choix_table > 0:
+    for i in range(1, choix_table + 1):
+        print(f"Table de multiplication de {i} :")
+        for j in range(1, 11):
+            resultat = i * j
+            print(f"{i} x {j} = {resultat}")
+        print("\n")
+else:
+    print("Nous sommes désolés, le chiffre n'est pas valide")
