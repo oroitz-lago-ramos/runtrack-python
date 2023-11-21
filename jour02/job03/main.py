defb40318a3eff72000ee8cb1d561df9f1647305
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-21 10:45:26 +0100

    Ameilleuration

--- jour02/job03/main.py
+++ jour02/job03/main.py
@@ -1,3 +1,8 @@
+# for i in range(0,101):
+#     if (i != 26) and (i != 37) and (i != 88):
+#         print(i)
+
+ban_list= [26, 37, 88]     
 for i in range(0,101):
-    if (i != 26) and (i != 37) and (i != 88):
+    if i not in ban_list:
         print(i)
