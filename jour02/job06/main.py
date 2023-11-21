c12096630cd44da2886c33918af6cda8d2acec89
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-21 12:08:07 +0100

    correction job06

--- jour02/job06/main.py
+++ jour02/job06/main.py
@@ -1,6 +1,6 @@
 for i in range(2, 1001):
     isPrime = True
-    for j in range(2, int(i ** (1/2)) + 1):
+    for j in range(2, int(i ** 0.5) + 1):
         if ((i / j) == int(i / j)):
             isPrime = False
     if i == 2:
