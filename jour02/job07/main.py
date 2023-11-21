8003508aa0e9a61e3d26c93122847ad3f809432f
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-21 14:51:51 +0100

    Correction Job07

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -1,3 +1,6 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for i in range(0,len(alphabet)):
-    print(alphabet[:i])
+alphabet = "abcdefghijklmnopqrstuvwxyz" * 10
+for i in range(0,len(alphabet),2):
+    if i == 0:
+        print(alphabet[0])
+    else:
+        print(alphabet[0:i + 1])
