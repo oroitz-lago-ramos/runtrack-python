2cd86d5d97cf7ad43003fce7c47bd0f7cce19fab
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-22 18:18:44 +0100

    Correction bug deciamal, une autre option serait gestionner que des int

--- jour03/job09/main.py
+++ jour03/job09/main.py
@@ -5,13 +5,17 @@ note1, note2, note3 = input("Entrez les trois notes séparés d'un espace: ").sp
 note1 = int(note1)
 note2 = int(note2)
 note3 = int(note3)
+print(note1,note2,note3)
 moyenne_etudiant = moyenne(note1,note2, note3)
+print(moyenne_etudiant)
 
-if 15 <= moyenne_etudiant <= 20:
+if 15.0 <= moyenne_etudiant <= 20.0:
     print("Très bon élève")
-elif 11 <= moyenne_etudiant <= 14:
+elif 11.0 <= moyenne_etudiant <= 14.99999999999:
     print("Bon élève")
-elif 8 <= moyenne_etudiant <= 10:
+elif 8.0 <= moyenne_etudiant <= 10.99999999999:
     print("Élève moyen")
-elif 0 <= moyenne_etudiant <= 7:
+elif 0.0 <= moyenne_etudiant <= 7.99999999999:
     print("Élève devant faire des efforts")
+else:
+    print("Nous sommes désole le format des notes n'est pas correct")
