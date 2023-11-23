53c1de32836ec3221bd574eed76a2b4bc3de0fd6
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-23 09:27:09 +0100

    Rendu plus prope, suppression de condition inutile

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -1,6 +1,3 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz" * 10
 for i in range(0,len(alphabet),2):
-    if i == 0:
-        print(alphabet[0])
-    else:
-        print(alphabet[0:i + 1])
+    print(alphabet[:i + 1])
