56419d1c6c0d71a342290c2692601de7c550055d
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-26 13:01:42 +0100

    ROund better

--- jour05/job06/main.py
+++ jour05/job06/main.py
@@ -1,5 +1,6 @@
 def distance_walked_per_week(steps, height):
     height_m = height / 100
-    print(f"Pour {steps} marches de {height} cm, le gardien parcourt {round(steps*height_m*5*2*7)} m par semaine.")
+    total_m = round(steps*height_m*5*2*7,2)
+    print(f"Pour {steps} marches de {height} cm, le gardien parcourt {total_m} m par semaine.")
     
-distance_walked_per_week(87,20)
+distance_walked_per_week(152,49)
